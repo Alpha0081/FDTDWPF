95b8a9bcc0bcdfa778b1f05e70bbea42e0f14aff
Павел <shmachilin@yandex.ru>
2019-11-10 19:25:33 +0300

    Универсальная команда

---
+++ Commands/LambdaCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FDTDWPF.Commands.Base;
+
+namespace FDTDWPF.Commands
+{
+    public class LambdaCommand : Command
+    {
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+
+        public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute)
+        {
+            _Execute = Execute;
+            _CanExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => _Execute(parameter);
+    }
+}
