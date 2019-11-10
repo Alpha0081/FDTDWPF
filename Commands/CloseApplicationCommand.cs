0290f2e5a19d030e272bd60dd1193be8f47d9a13
Павел <shmachilin@yandex.ru>
2019-11-10 19:30:17 +0300

    Пример создания новой отдельной команды

---
+++ Commands/CloseApplicationCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using FDTDWPF.Commands.Base;
+
+namespace FDTDWPF.Commands
+{
+    public class CloseApplicationCommand : Command
+    {
+        public int ResultCode { get; set; }
+
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter) => Application.Current.Shutdown(ResultCode);
+    }
+}
