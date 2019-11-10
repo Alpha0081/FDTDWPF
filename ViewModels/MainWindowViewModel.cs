e3b4a429d87937c1a48e10a0e4867f114ccb11e1
Павел <shmachilin@yandex.ru>
2019-11-10 19:19:44 +0300

    Вью-модель главного окна + первая привязка к свойствоу заголовка

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using FDTDWPF.ViewModels.Base;
+
+namespace FDTDWPF.ViewModels
+{
+    public class MainWindowViewModel : ViewModel
+    {
+        #region Title : string - Заголовок окна
+
+        /// <summary>Заголовок окна</summary>
+        private string _Title = "Главное окно";
+
+        /// <summary>Заголовок окна</summary>
+        public string Title { get => _Title; set => Set(ref _Title, value); }
+
+        #endregion
+    }
+}
